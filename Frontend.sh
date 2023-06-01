5f4ac16b65fe5d81dd5efdf368fd0790b7a87629
imrangithubrit <devopsandaws2023@gmail.com>
2023-06-01 16:05:56 +0530

    Add color in fronend

--- Frontend.sh
+++ Frontend.sh
@@ -1,17 +1,18 @@
-
+echo -e "\e[33m Installing the ngix\e[0m"
 yum install nginx -y 
 
- 
-
-
+echo -e "\e[33m Removing the old app content\e[0m"
 rm -rf /usr/share/nginx/html/* 
 
+echo -e "\e[33m Downloading the frontend\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
-
-
+ 
+echo -e "\e[33m Extract the frontend content\e[0m"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
 # create new roboshop.conf file
+
+echo -e "\e[33m starting the nginx server\e[0m"
 systemctl enable nginx
 systemctl restart nginx 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # project
+
+
+echo -e "\e[33m Installing the ngix\e[0m"
