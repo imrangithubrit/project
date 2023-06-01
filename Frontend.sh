7972db6cb380d8f66145bf3e402d2e6019875e43
imrangithubrit <devopsandaws2023@gmail.com>
2023-06-01 16:08:42 +0530

    No out put

--- Frontend.sh
+++ Frontend.sh
@@ -1,18 +1,18 @@
-echo -e "\e[33m Installing the ngix\e[0m"
-yum install nginx -y 
+echo -e "\e[33m Installing the ngix\e[0m" 
+yum install nginx -y  &>> /tmp/roboshop.log
 
 echo -e "\e[33m Removing the old app content\e[0m"
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/*   &>> /tmp/roboshop.log
 
 echo -e "\e[33m Downloading the frontend\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>> /tmp/roboshop.log
  
 echo -e "\e[33m Extract the frontend content\e[0m"
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html  &>> /tmp/roboshop.log
+unzip /tmp/frontend.zip  &>> /tmp/roboshop.log
 
 # create new roboshop.conf file
 
 echo -e "\e[33m starting the nginx server\e[0m"
-systemctl enable nginx
-systemctl restart nginx 
+systemctl enable nginx &>> /tmp/roboshop.log
+systemctl restart nginx  &>> /tmp/roboshop.log
