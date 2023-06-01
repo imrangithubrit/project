f6d84945a75733b1757fb4287194199f5d40b36c
imrangithubrit <devopsandaws2023@gmail.com>
2023-06-01 16:00:34 +0530

    sdsa

--- Frontend.sh
+++ Frontend.sh
@@ -1,16 +1,17 @@
+
 yum install nginx -y 
 
  
+
+
 rm -rf /usr/share/nginx/html/* 
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 
+
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
-
-# Need to create Roboshop.conf file
-
-
+# create new roboshop.conf file
 systemctl enable nginx
 systemctl restart nginx 
