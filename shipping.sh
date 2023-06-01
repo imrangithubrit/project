662f446a87d18bd980d4fd6aea40750ea37de81a
imrangithubrit <devopsandaws2023@gmail.com>
2023-06-01 19:39:46 +0530

    sd

---
+++ shipping.sh
@@ -0,0 +1,29 @@
+
+yum install maven -y
+
+useradd roboshop
+
+mkdir /app 
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip 
+cd /app 
+unzip /tmp/shipping.zip
+
+cd /app 
+mvn clean package 
+mv target/shipping-1.0.jar shipping.jar 
+
+# copy shipping
+cp /root/project/shipping.service /etc/systemd/system/user.service
+
+systemctl daemon-reload
+
+systemctl enable user 
+systemctl restart user
+
+#copy the mongob file
+cp /root/project/mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+yum install mongodb-org-shell -y
+
+mongo --host mongodb-dev.devopsb72.store </app/schema/user.js
