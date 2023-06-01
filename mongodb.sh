88cfc93849ac8635943b226fe09dd1cc8c58fb62
imrangithubrit <devopsandaws2023@gmail.com>
2023-06-01 16:31:26 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,11 @@
 #copy mongodb.repo file/root/project
+echo -e "\e[33m Copy the Mongo.repo file\e[0m"
+cp /root/project/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 
-cp /root/project/mongodb.repo /etc/yum.repos.d/mongodb.repo
-
-
-yum install mongodb-org -y 
+echo -e "\e[33m Installing the mongodb file\e[0m"
+yum install mongodb-org -y  &>>/tmp/roboshop.log
 
 #Modify the confg file
-
-systemctl enable mongod 
-systemctl restart mongod
+echo -e "\e[33m starting the nginx server\e[0m"
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
