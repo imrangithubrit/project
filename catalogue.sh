af0677cd5f11d3f44d24c74129ce00e1c4d48ad3
imrangithubrit <devopsandaws2023@gmail.com>
2023-06-03 13:13:19 +0530

    dcd

--- catalogue.sh
+++ catalogue.sh
@@ -4,9 +4,4 @@ component=catalogue
 
 nodejs
 
-echo -e "${color} copy mongo db repo file ${nocolor}" 
-cp /root/project/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>> /tmp/roboshop.log
-
-echo -e "${color} Install mongodb ${nocolor}" 
-yum install mongodb-org-shell -y  &>> /tmp/roboshop.log
-mongo --host mongodb-dev.devopsb72.store </app/schema/$component.js   &>> /tmp/roboshop.log
+mongodb_schema setup
--- common.sh
+++ common.sh
@@ -36,3 +36,14 @@ nodejs()  {
  systemctl enable $component &>> /tmp/roboshop.log
  systemctl restart $component  &>> /tmp/roboshop.log
 }
+
+
+mongodb_schema setup() {
+
+ echo -e "${color} copy mongo db repo file ${nocolor}" 
+ cp /root/project/mongodb.repo /etc/yum.repos.d/mongodb.repo  &>> /tmp/roboshop.log
+
+ echo -e "${color} Install mongodb ${nocolor}" 
+ yum install mongodb-org-shell -y  &>> /tmp/roboshop.log
+ mongo --host mongodb-dev.devopsb72.store </app/schema/$component.js   &>> /tmp/roboshop.log
+ }
