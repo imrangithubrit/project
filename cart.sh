1fbd9a3f25c0a4ffa017e891b70797393f455c86
imrangithubrit <devopsandaws2023@gmail.com>
2023-06-03 12:39:26 +0530

    csad

--- cart.sh
+++ cart.sh
@@ -1,32 +1,33 @@
-echo -e "\e[33m Configuring Nodejs repo\e[0m" 
+source common.sh
+echo -e "$color Configuring Nodejs repo $nocolor" 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>> /tmp/roboshop.log
 
-echo -e "\e[33m Installing Nodejs \e[0m" 
+echo -e "$color Installing Nodejs $nocolor" 
 yum install nodejs -y   &>> /tmp/roboshop.log
 
-echo -e "\e[33m Added Application user\e[0m" 
+echo -e "$color Added Application user $nocolor" 
 useradd roboshop   &>> /tmp/roboshop.log
 
-echo -e "\e[33m create app directory\e[0m"
+echo -e "$color create app directory $nocolor"
 rm -rf /app   &>> /tmp/roboshop.log
 mkdir /app   &>> /tmp/roboshop.log
 
-echo -e "\e[33m download application content\e[0m" 
+echo -e "$color download application content $nocolor" 
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip   &>> /tmp/roboshop.log
 
-echo -e "\e[33m Extract Application content\e[0m" 
+echo -e "$color Extract Application content $nocolor" 
 cd /app    &>> /tmp/roboshop.log
 unzip /tmp/cart.zip   &>> /tmp/roboshop.log
 
 
 cd /app  &>> /tmp/roboshop.log
-echo -e "\e[33m install nodejs dependencies\e[0m"
+echo -e "$color install nodejs dependencies $nocolor"
 npm install   &>> /tmp/roboshop.log
 
-echo -e "\e[33m copying in the cart service\e[0m"
+echo -e "$color copying in the cart service $nocolor"
 cp /root/project/cart.service /etc/systemd/system/cart.service  &>> /tmp/roboshop.log
 
-echo -e "\e[33m setup systemd services\e[0m" 
+echo -e "$color setup systemd services $nocolor" 
 systemctl daemon-reload &>> /tmp/roboshop.log
 systemctl enable cart  &>> /tmp/roboshop.log
 systemctl restart cart &>> /tmp/roboshop.log
