106e71e99eb2c10d6bc3f9a2f248a09d7b9ab673
imrangithubrit <devopsandaws2023@gmail.com>
2023-06-03 13:07:37 +0530

    dcd

--- common.sh
+++ common.sh
@@ -1,2 +1,38 @@
 color="\e[36m"
 nocolor="\e[0m"
+
+
+nodejs()  {
+  echo -e "${color} Configuring Nodejs repo ${nocolor}" 
+
+ curl -sL https://rpm.nodesource.com/setup_lts.x | bash   &>> /tmp/roboshop.log
+
+ echo -e "${color} Installing Node js ${nocolor}" 
+ yum install nodejs -y      &>> /tmp/roboshop.log
+
+ echo -e "${color} Added Application user ${nocolor}" 
+ useradd roboshop   &>> /tmp/roboshop.log
+
+ echo -e "${color} create app directory ${nocolor}"
+ rm -rf /app   &>> /tmp/roboshop.log
+ mkdir /app    &>> /tmp/roboshop.log
+
+ echo -e "${color} download application content ${nocolor}" 
+ curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip   &>> /tmp/roboshop.log 
+
+ echo -e "${color} Extract Application content ${nocolor}" 
+ cd /app   &>> /tmp/roboshop.log
+ unzip /tmp/$component.zip   &>> /tmp/roboshop.log
+
+ cd /app  &>> /tmp/roboshop.log
+ echo -e "${color} install nodejs dependencies ${nocolor}"
+ npm install  &>> /tmp/roboshop.log
+ # Need to copy the catalogu.service file
+ echo -e "${color} setup systemd services ${nocolor}" 
+ cp /root/project/$component.service  /etc/systemd/system/$component.service &>> /tmp/roboshop.log
+
+ echo -e "${color} starts $component services ${nocolor}" 
+ systemctl daemon-reload  &>> /tmp/roboshop.log
+ systemctl enable $component &>> /tmp/roboshop.log
+ systemctl restart $component  &>> /tmp/roboshop.log
+}
